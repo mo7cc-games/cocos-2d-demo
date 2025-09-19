b1b635bc96734f523bbd2f86a6fa100b9e021dbc
墨七 <meichangliang@outlook.com>
2025-09-19 16:37:31 +0800

    x

---
+++ assets/HelloWorld.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("HelloWorld")
+export class HelloWorld extends Component {
+  start() {
+    console.info("Hello world222");
+  }
+
+  update(deltaTime: number) {}
+}
