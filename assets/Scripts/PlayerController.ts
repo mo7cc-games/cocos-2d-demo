cad1c60372d876585e78584c9b61545b817456d2
墨七 <meichangliang@outlook.com>
2025-09-24 16:00:55 +0800

    创建脚本

---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
